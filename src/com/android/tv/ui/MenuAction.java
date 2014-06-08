8014e1bca6113496da6085e655f331b0d7f335f2
Chulwoo Lee <chulwoo@google.com>
2014-06-08 19:41:35 +0900

    Applied new menu icons from UX
    BUG: 15484942
    Change-Id: I17204f2689e5d34909833adf313ea8058e46008a

--- src/com/android/tv/ui/MenuAction.java
+++ src/com/android/tv/ui/MenuAction.java
@@ -42,7 +42,7 @@ public class MenuAction {
                     R.drawable.ic_tvoption_aspect);
     public static final MenuAction SELECT_TV_INPUT_ACTION =
             new MenuAction(R.string.menu_select_input, SELECT_TV_INPUT_TYPE,
-                    R.drawable.ic_tvoptions_input);
+                    R.drawable.ic_tvoptions_input_settings);
     public static final MenuAction TOGGLE_PIP_ACTION =
             new MenuAction(R.string.menu_toggle_pip, TOGGLE_PIP_TYPE,
                     R.drawable.ic_tvoption_pip);
@@ -50,10 +50,10 @@ public class MenuAction {
     // TODO: UX is not ready.
     public static final MenuAction EDIT_CHANNEL_LIST_ACTION =
             new MenuAction(R.string.menu_edit_channels, EDIT_CHANNEL_LIST_TYPE,
-                    R.drawable.ic_tvoption_more);
+                    R.drawable.ic_tvoptions_edit_channels);
     public static final MenuAction AUTO_SCAN_CHANNELS_ACTION =
             new MenuAction(R.string.menu_auto_scan, AUTO_SCAN_CHANNELS_TYPE,
-                    R.drawable.ic_tvoption_more);
+                    R.drawable.ic_tvoptions_scan);
     public static final MenuAction INPUT_SETTING_ACTION =
             new MenuAction(R.string.menu_input_setting, INPUT_SETTING_TYPE,
                     R.drawable.ic_tvoption_more);
